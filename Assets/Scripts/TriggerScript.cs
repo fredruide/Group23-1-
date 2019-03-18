a27a4ddb444a18250fb99bb7f803fd580b7f04f0
DrMoJones <jonashc@live.dk>
2019-03-18 12:05:13 +0100

    Jonas Initilize
    Jonas

---
+++ Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour
+{
+    GoombaScript GoombaScript;
+    Rigidbody2D rb;
+
+
+    private void Start()
+    {
+        GoombaScript = GetComponentInParent<GoombaScript>();
+        rb = GetComponentInParent<Rigidbody2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D wall)
+    {
+        print(rb.gameObject.name + ": Trigger Enter" );
+
+        if (wall.gameObject.tag == "Wall")
+        {
+            GoombaScript.terrainTrigger = wall;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D drop)
+    {
+        print(rb.gameObject.name + ": Trigger Exit");
+
+        if (drop.gameObject.tag == "Platform")
+        {
+            GoombaScript.terrainTrigger = drop;
+        }
+        
+    }
+}
