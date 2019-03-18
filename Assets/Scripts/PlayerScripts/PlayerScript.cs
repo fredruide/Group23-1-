10cf25e27f9e67ecfc1fa563aa3ddf4faaed1ef2
Fredruide <freddyagnb@gmail.com>
2019-03-18 12:46:23 +0100

    Player script prefap
    PLayerscript with

---
+++ Assets/Scripts/PlayerScripts/PlayerScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    Animator ani;
+
+    [HideInInspector]
+    public Collider2D leftTrigger;
+    [HideInInspector]
+    public Collider2D rightTrigger;
+    [HideInInspector]
+    public Collider2D bottomTrigger;
+    [HideInInspector]
+    public Collider2D topTrigger;
+
+    bool grounded;
+    bool hasBeenGrounded;
+
+    #region IDEvariabler
+    public float speed;
+    public float jump;
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        ani = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        grounded = true ? bottomTrigger.gameObject.tag == "Ground" : false;
+
+        ani.SetBool("Grounded", grounded);
+
+        HorizontalMovement();
+        Jump();
+
+        //print("grounded: " + grounded);
+        //print("hasBeen: " + hasBeenGrounded);
+        //print("input: " + Input.GetButtonDown("Vertical"));
+    }
+
+    void HorizontalMovement()
+    {
+        if (grounded && Input.GetAxisRaw("Horizontal") != 0)
+        {
+            rb.velocity = new Vector2(speed * Input.GetAxisRaw("Horizontal"), rb.velocity.y);
+        }
+        else if (!grounded && Input.GetAxisRaw("Horizontal") != 0)
+        {
+            rb.velocity = new Vector2(speed / 2 * Input.GetAxisRaw("Horizontal"), rb.velocity.y);
+        }
+        else if (Input.GetAxisRaw("Horizontal") == 0)
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+    }
+
+    void Jump()
+    {
+        if (grounded && Input.GetButtonDown("Vertical"))
+        {
+            print("jump");
+            rb.velocity = new Vector2(rb.velocity.x, jump);
+            ani.SetBool("Jumping", true);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/TriggerScripts/LeftTriggerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftTriggerScript : MonoBehaviour
+{
+    //Rigidbody2D rb;
+    PlayerScript ps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody2D>();
+        ps = GetComponentInParent<PlayerScript>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ps.leftTrigger = collision;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        ps.leftTrigger = collision;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ps.leftTrigger = collision;
+    }
+}
