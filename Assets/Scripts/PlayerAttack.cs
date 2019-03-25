bff6692076c27d79fbbc816febff056e69219828
BigDickJohnson <danielp2004@msn.com>
2019-03-25 12:04:30 +0100

    Player attack | Basic Enemy | Cinematic
    Har added et basic attack script (Skal laves til 3 combo
    
    Har lavet en basic enemy "Grøn blok" med HP
    
    Har added Cinematic som jeg ville teste til kameraet

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public Transform attackPos;
+    public LayerMask whatisEnemy;
+    public float attackRange;
+    public int damage;
+
+    void Update()
+    {
+        if(timeBtwAttack <= 0)
+        {
+
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+            timeBtwAttack = startTimeBtwAttack;
+            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatisEnemy);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                    {
+                    enemiesToDamage[i].GetComponent<EnemyTest>().TakeDmg(damage);
+                    }
+            }
+
+        }
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
