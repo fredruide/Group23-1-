20cee55125b6a93c2632acdd95f34a02fa9e6ed6
DrMoJones <jonashc@live.dk>
2019-03-25 10:29:10 +0100

    Gator1

---
+++ Assets/GatorMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GatorMove : MonoBehaviour
+{
+    GameObject playerObject;
+
+    public float Speed;
+
+    public float playerRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerObject = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, playerObject.transform.position, Speed * Time.deltaTime); 
+    }
+}
