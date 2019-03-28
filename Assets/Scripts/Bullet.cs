5b05a5c954851a8bd516a72b490958ff84791201
BigDickJohnson <danielp2004@msn.com>
2019-03-28 13:42:24 +0100

    ælæl

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,19 +4,4 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public Transform firePoint;
-    public GameObject bullet;
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.V))
-        {
-            Fire();
-        }
-    }
-
-    void Fire()
-    {
-        Instantiate(bullet, firePoint.position, firePoint.rotation);
-    }
-
 }
