487c3ae57e5b1828b5233e4eded556788527d3da
Jakob Vestergaard <Jakobmetalv@gmail.com>
2019-04-23 09:55:21 +0200

    Housenodes:01
    e

--- Assets/Scripts/HouseScripts/ExampleScript.cs
+++ Assets/Scripts/HouseScripts/ExampleScript.cs
@@ -2,31 +2,18 @@
 
 public class ExampleScript : MonoBehaviour
 {
-    // Create 10 game objects, which will have random Instance IDs
-    void Awake()
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            GameObject g = new GameObject("abc" + i.ToString("D3"));
-        }
-    }
-
-    // Find all the game objects and display their Instance IDs
-    void Start()
-    {
+    //// Find all the game objects and display their Instance IDs
+    //void Start()
+    //{
         
-    }
+    //}
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            Object[] allObjects = Object.FindObjectsOfType<GameObject>();
+    //private float peter;
 
-            foreach (GameObject go in allObjects)
-            {
-                Debug.Log(go + " is an active object " + go.GetInstanceID());
-            }
-        }
-    }
+    //void Update()
+    //{
+
+    //    peter += Time.deltaTime;
+    //    Debug.Log(peter);  
+    //}
 }
--- Assets/Scripts/HouseScripts/HouseNodes.cs
+++ Assets/Scripts/HouseScripts/HouseNodes.cs
@@ -42,7 +42,7 @@ public class HouseNodes : MonoBehaviour
         }   
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
 
         if (collision.gameObject.tag == "Player")
