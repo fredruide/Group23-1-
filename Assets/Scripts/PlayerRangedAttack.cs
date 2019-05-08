2607f3e11dec41a4bc554fe06c5616f64f7697d8
BigDickJohnson <danielp2004@msn.com>
2019-05-08 12:38:43 +0200

    Dont know

--- Assets/Scripts/PlayerRangedAttack.cs
+++ Assets/Scripts/PlayerRangedAttack.cs
@@ -4,4 +4,47 @@ using UnityEngine;
 
 public class PlayerRangedAttack : MonoBehaviour
 {
+    public Transform firePoint;
+    public GameObject bullet;
+    public int ammo;
+    public float reloadTime;
+    public float startReloadTime;
+    public float mousePosition;
+    void Update()
+    {
+        if (reloadTime <= 0)
+        {
+            if ((Input.GetKeyDown(KeyCode.V) && ammo > 0) && reloadTime < 0)
+            {
+                Fire();
+                ammo--;
+            }
+            else if (Input.GetKeyDown(KeyCode.V) && ammo == 0)
+            {
+                reload();
+            }
+        }
+        else
+        {
+            reloadTime -= Time.deltaTime;
+        }
+        if (reloadTime <= 0)
+        {
+            PlayerScript.canMoveTest = true;
+        }
+
+    }
+
+    void Fire()
+    {
+        Instantiate(bullet, firePoint.position, firePoint.rotation);
+    }
+
+    void reload()
+    {
+        reloadTime = startReloadTime;
+        ammo = 1;
+        PlayerScript.canMoveTest = false;
+        Debug.Log("Reloading");
+    }
 }
