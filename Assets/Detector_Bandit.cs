9a1ebb4ab47983a21e2b9aa3c058a7ff81851337
Fredruide <freddyagnb@gmail.com>
2019-06-19 14:52:45 +0200

    Elev Emils mini project
    Folkeskole eleven Emil lavede et script til at få et object til at gå frem og tilbage samt et næsten fungtionel method for at få objected til at følge efter player objected

---
+++ Assets/Detector_Bandit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detector_Bandit : MonoBehaviour
+{
+    movement movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = GetComponentInParent<movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "Player")
+        {
+            movement.detector = true;
+            //movement.velocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            movement.detector = false;
+    }
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    Rigidbody2D body;
+    BoxCollider2D boxcollider;
+    BoxCollider2D childbox;
+    float movementcounter = 0;
+    public bool detector;
+
+    
+    //public Vector2 velocity = Vector2.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        boxcollider = GetComponent<BoxCollider2D>();
+        childbox = GetComponentInChildren<BoxCollider2D>();
+        body.velocity = new Vector2(7, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(body.velocity);
+        //Debug.Log(movementcounter);
+        Debug.Log(detector);
+        //velocity = new Vector2(5, 0);
+        movementcounter += Time.deltaTime;
+        if (detector == true)
+        {
+            //body.transform.position = Vector2.SmoothDamp(body.transform.position, GameObject.Find("Player").transform.position, ref velocity, 7);
+
+            transform.position = Vector2.MoveTowards(transform.position, GameObject.Find("Player").transform.position, 0.01f);
+        }
+        else
+        {
+            if (movementcounter < 5)
+            {
+                body.velocity = new Vector2(7, 0);
+
+            }
+            else
+            {
+                body.velocity = new Vector2(-7, 0);
+            }
+            if (movementcounter >= 10)
+            {
+                movementcounter = 0;
+            }
+        }
+
+    }
+
+}
